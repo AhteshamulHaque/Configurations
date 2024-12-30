95fb49e59096f11a97e6030819c905eb63ee6f48
AhteshamulHaque <ahteshamul8900@gmail.com>
2024-12-31 02:34:18 +0530

    Nvim session restore function fixed

--- tmux/nvim_session.sh
+++ tmux/nvim_session.sh
@@ -5,19 +5,18 @@
 # Same as vim strategy, see file 'vim_session.sh'
 
 ORIGINAL_COMMAND="$1"
-SESSION_LOCATION="/home/ahteshamul/.local/state/nvim/sessions"
-TEST_DIRECTORY=`echo $2 | sed 's/\//%/g'`
-LAUNCH_DIRECTORY=`echo $2 | sed 's/\//\\\%/g'`
+SESSION_LOCATION="~/.local/state/nvim/sessions"
+SESSION_NAME=`echo $2 | sed 's/\//%/g'`
+TEST_FILE_LOCATION="${SESSION_LOCATION}/${SESSION_NAME}.vim"
+ORIGINAL_FILE_LOCATION=`echo ${TEST_FILE_LOCATION} | sed 's/\%/\\\%/g'`
 
 original_command_contains_session_flag() {
 	[[ "$ORIGINAL_COMMAND" =~ "-S" ]]
 }
 
 main() {
-	if [ -f "${SESSION_LOCATION}/${TEST_DIRECTORY}.vim" ]; then
-		echo "nvim -S \"${SESSION_LOCATION}/${LAUNCH_DIRECTORY}.vim\""
-	elif [ -f "${SESSION_LOCATION}/${TEST_DIRECTORY}%%nvim-restore.vim" ]; then
-    echo "nvim -S \"${SESSION_LOCATION}/${LAUNCH_DIRECTORY}\%\%nvim-restore.vim\""
+	if [ ! -e "${TEST_FILE_LOCATION}" ]; then
+		echo "nvim -S \"${ORIGINAL_FILE_LOCATION}\""
 	elif original_command_contains_session_flag; then
 		# Session file does not exist, yet the original nvim command contains
 		# session flag `-S`. This will cause an error, so we're falling back to
